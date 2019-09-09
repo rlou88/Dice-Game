3d027efb50c7186c718dfe10cb9a49b7c0170754
Ronald <ronald.lou88@gmail.com>
2019-09-09 13:57:30 -0500

    Dice game

---
+++ index.js
@@ -0,0 +1,25 @@
+let randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+
+const randomDiceImage = "dice" + randomNumber1 + ".png"; // dice1.png - dice6.png
+
+const randomImageSource = "images/" + randomDiceImage; // img/dice1.png - img/dice6.png
+
+const image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+const randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+const randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+}
+else {
+  document.querySelector("h1").innerHTML  = "Draw!";
+}
